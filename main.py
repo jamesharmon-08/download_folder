76e0522e3069ca7e21eaa7472627b28c48221774
jamesharmon <james.harmon@outlook.com>
2021-08-09 21:24:12 -0400

    Moved folders to a backup folder

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 DOWNLOADS = "/Volumes/3d printing"
 
 only_files = [f for f in listdir(DOWNLOADS) if isfile(join(DOWNLOADS, f))]
-
+only_dirs = [d for d in listdir(DOWNLOADS) if isdir(join(DOWNLOADS, d))]
 for file in only_files:
     name, extension = splitext(join(DOWNLOADS,file))
     directory = extension
@@ -23,3 +23,15 @@ for file in only_files:
     else:    #  Delete older files
         remove(join(DOWNLOADS, file))
 
+# ## Create backup folder ## #
+backup_folder = datetime.now().strftime("backup-%d-%m-%Y")
+try:
+    mkdir(join(DOWNLOADS,backup_folder))
+except FileExistsError:
+    print("Folder exists")
+for dir in only_dirs:    # Move folders into a backup folder
+    if 'backup' != dir.split("-")[0]:  # move folders to back-up folder
+        try:
+            rename(join(DOWNLOADS,dir),join(DOWNLOADS,backup_folder,dir))
+        except PermissionError:
+            print("Permission Error:  System or Hidden Folder")
